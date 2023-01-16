0228bd0a9b7f534fa68b0e47fa2ab6af4ff3d8e9
unknown <34804@ma-web.nl>
2023-01-16 10:27:24 +0100

    _

--- les1/app.js
+++ les1/app.js
@@ -4,13 +4,13 @@ class App
     {
         let title = document.getElementById(titleIp);
         let randomGetal = math.random();
-        if (randomGetal == 0);
+        if (randomGetal == 0)
         {
             console.log("Getal is 0");
         }
-        else if (randomGetal <= 0.2);
+        else if (randomGetal <= 0.2)
         {
-            console.log("getal is kleiner dan 0.2")
+            console.log("getal is kleiner dan 0.2");
         }
     }
     
